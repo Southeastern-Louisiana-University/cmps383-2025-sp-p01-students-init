227825ad534dea73674c50845523183a5fd5cb34
Ajyol Dhamala <ajyold112@gmail.com>
2025-02-04 10:13:39 -0600

    Movie, Review, Showtime entites created.

---
+++ Selu383.SP25.Api/Entity/Movie.cs
@@ -0,0 +1,40 @@
+﻿using Selu383.SP25.Api.Data.Base;
+
+namespace Selu383.SP25.Api.Entity
+{
+    public class Movie : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Review Review { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public string ImageUrl { get; set; }
+        public string TrailerUrl { get; set; }
+        public List<Showtime> Showtimes { get; set; }
+    }
+
+    public class MovieGetDto
+    {
+        public int Id { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public string ImageUrl { get; set; }
+        public string TrailerUrl { get; set; }
+        public List<Showtime> Showtimes { get; set; }
+    }
+
+    public class MovieCreateDto
+    {
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public string ImageUrl { get; set; }
+        public string TrailerUrl { get; set; }
+    }
+}
---
+++ Selu383.SP25.Api/Entity/Review.cs
@@ -0,0 +1,46 @@
+﻿using Selu383.SP25.Api.Data.Base;
+
+namespace Selu383.SP25.Api.Entity
+{
+    public class Review : IEntityBase
+    {
+        public int Id { get; set; }
+        public string review { get; set; }
+        public int Rating { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public Movie Movie { get; set; }
+        public int MovieId { get; set; }
+
+    }
+
+    public class ReviewsGetDto
+    {
+        public int Id { get; set; }
+        public string review { get; set; }
+        public int Rating { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+
+    }
+
+    public class ReviewsCreateDto
+    {
+        public string Review { get; set; }
+        public int Rating { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+
+    }
+
+    public class ReviewsUpdateDto
+    {
+        public int Id { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+        public int MovieId { get; set; }
+
+    }
+}
