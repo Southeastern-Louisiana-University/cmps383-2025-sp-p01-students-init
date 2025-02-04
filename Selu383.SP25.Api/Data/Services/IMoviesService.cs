f089daf9c23f299bb39b758f51275bc5f08749d6
Ajyol Dhamala <ajyold112@gmail.com>
2025-02-04 10:14:14 -0600

    Movie, Review services created

---
+++ Selu383.SP25.Api/Data/Services/IMoviesService.cs
@@ -0,0 +1,9 @@
+﻿using Selu383.SP25.Api.Data.Base;
+using Selu383.SP25.Api.Entity;
+
+namespace Selu383.SP25.Api.Data.Services
+{
+    public interface IMoviesService : IEntityBaseRepository<Movie>
+    {
+    }
+}
