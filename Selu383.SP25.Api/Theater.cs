cc7063d9403a8d0f04f1cb5aa6efc997908ccfac
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-02-05 20:31:54 -0600

    Reapply "TheaterDto, TheaterController added with Migration and Data seeded"
    This reverts commit 15f83db9b744844f6f66dd8ad172319eb8804fd2.

---
+++ Selu383.SP25.Api/Theater.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Selu383.SP25.Api
+{
+    public class Theater
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(120)]
+        public string Name { get; set; }    
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int SeatCount { get; set; }
+    }
+}
